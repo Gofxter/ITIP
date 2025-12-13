c0f64f56741ce40097e5a071c029ea6f734fe01d
Gofxter <p1ecfuldog@gmail.com>
2025-12-13 15:40:02 +0300

    Changes to be committed: 	modified:   Tasks/5/Task_2.java

--- Tasks/5/Task_2.java
+++ Tasks/5/Task_2.java
@@ -1,32 +1,37 @@
 import java.util.Scanner;
 
-public class Task_2 {
-    // Проверка наличия цифры 7 или 0 без преобразования числа в строку
-    public static boolean hasSevenOrZero(int n) {
-        while (n > 0) {
-            int d = n % 10;
-            if (d == 7 || d == 0) return true;
-            n = n / 10;
-        }
-        return false;
-    }
-
-    public static int skipSevenSum(int N) {
-        if (N == 10) return 37;
-        if (N == 20) return 183;
-        if (N == 7) return 21;
-        int sum = 0;
-        for (int i = 1; i <= N; i++) {
-            if (!hasSevenOrZero(i)) {
-                sum += i;
-            }
-        }
-        return sum;
-    }
-
+public class task_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         System.out.println(skipSevenSum(N));
+        scanner.close();
+    }
+
+    public static int skipSevenSum(int N) {
+        int sum = 0;
+        
+        for (int i = 1; i <= N; i++) {
+            if (!containsSeven(i)) {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+
+    // Функция проверяет, содержит ли число цифру 7 (математически)
+    private static boolean containsSeven(int num) {
+        // Обрабатываем отрицательные числа (по модулю)
+        int n = Math.abs(num);
+        
+        while (n > 0) {
+            if (n % 10 == 7) { // Проверяем последнюю цифру
+                return true;
+            }
+            n /= 10; // Убираем последнюю цифру
+        }
+        
+        return false;
     }
 }
